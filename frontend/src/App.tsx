91eb39d1ff26877ba14f60283b1fdf256ac0510e
Laurenz1606 <laurenz.rausche@gmail.com>
2022-11-19 11:05:56 +0000

    added tailwind + heroicons + headlessui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Hallo</header>
     </div>
   );
 }
