a0fc19a8aae3c1cf9e9a089296063e19ff700b8d
Laurenz1606 <laurenz.rausche@gmail.com>
2022-11-19 12:11:53 +0000

    added dynamic backend url fetching in frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,46 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+//create async init function
+async function init() {
+  //define url (default at first to null)
+  let url: string | null = null;
+
+  //check if in dev mode
+  if (
+    process.env.NODE_ENV === "development" &&
+    process.env.REACT_APP_DEV_BACKEND
+  ) {
+    //set url to env var defined in dev mode
+    url = process.env.REACT_APP_DEV_BACKEND;
+  } else {
+    try {
+      //fetch url from /api_url route
+      const fetchedUrl: { url: string } = await fetch("/api_url").then((res) =>
+        res.json(),
+      );
+
+      //set url to fetched url
+      url = fetchedUrl.url;
+    } catch (err) {
+      //log error
+      console.log(err);
+    }
+  }
+
+  //check if url was found
+  if (url) {
+    //render react
+    const root = ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement,
+    );
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  }
+}
+
+//call init function
+init();
